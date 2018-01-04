229bf59392e5147a491d5f032d2183f784a562e0
masterloutre <williejunpow@gmail.com>
2018-01-04 13:58:00 +0100

    cameras OK

--- imacman/src/Camera2D.cpp
+++ imacman/src/Camera2D.cpp
@@ -24,10 +24,10 @@ void Camera2D::moveFront(float t){
 
 glm::mat4 Camera2D::getViewMatrix() const{
     glm::mat4 viewMatrix = glm::mat4(1.f);
-
     viewMatrix = glm::rotate(viewMatrix, glm::radians(m_fAngleX), glm::vec3(1,0,0));
+    viewMatrix = glm::rotate(viewMatrix, glm::radians(-m_fAngleY-180), glm::vec3(0,1,0));
     viewMatrix = glm::translate(viewMatrix,glm::vec3(-positionX, -m_fDistance, -positionY));
-    //viewMatrix = glm::rotate(viewMatrix, glm::radians(-m_fAngleY), glm::vec3(0,1,0));
+
 
     return viewMatrix;
 }
