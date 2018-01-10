bf1f8f08b066d296274218cd873c3611932b8c79
Amel <amel.negra@gmail.com>
2018-01-10 02:01:47 +0100

    Dijkstra Ghost AI

--- imacman/include/Ghost.hpp
+++ imacman/include/Ghost.hpp
@@ -26,10 +26,9 @@ public:
     void returnToStartPos();
     void setScaredState();
     //returns vector of path from s to d
-    static std::vector<Tile *> isReachable(Tile s, Tile d, std::vector<std::vector<Tile>> tiles);
     void createRenderModel();
     void render();
-
+    int Dijkstra(int A, int B, std::vector<std::vector<int>> neighbours);
     const glm::vec2 &getPosition() const;
 
 private:
